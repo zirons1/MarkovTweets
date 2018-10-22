3793f254063f76cbf36486f4c38e396b0ca35ab0
zirons1 <zirons@udel.edu>
2018-10-22 14:41:25 -0400

    Update json_reader.py

--- json_reader.py
+++ json_reader.py
@@ -1,22 +1,37 @@
-import configparser
-import requests
-import base64
+import tweepy
+import json
+import sys
 
-consumer_key = 'R7UdLqOefMzHND0gJ8k7cV9tw'
-consumer_secret = 'C41jYgKDTYiAJwlnBnumoENFBrikglnH3HKB6qxRkRoViPbWML'
+file = open("tweets.txt", "w")
 
-key_secret = base64.urlsafe_b64encode('{}:{}'.format(consumer_key, consumer_secret).encode('ascii')).decode('ascii')
+consumer_key = 'XXXXXXXXXXXXXXXXXXXX'
+consumer_secret = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+access_token = 'XXXXXXXXXX-XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
+access_token_secret = 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'
 
-base_url = 'https://api.twitter.com/'
-auth_url = '{}oauth2/token'.format(base_url)
-auth_headers = {
-    'Authorization': 'Basic {}'.format(key_secret),
-    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-}
-auth_data = {
-    'grant_type': 'client_credentials'
-}
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
 
-auth_resp = requests.post(auth_url, headers=auth_headers, data=auth_data)
-print(auth_resp.status_code)
-print(resp.status, resp.reason)
+api = tweepy.API(auth)
+
+tweet_counter = 0
+
+handle = str(sys.argv[1])
+
+counter = 0
+tweets = api.user_timeline(screen_name=handle, count=200, include_rts = False, tweet_mode='extended')
+
+while(counter < 15):
+	last = {}
+
+	for tweet in tweets:
+		tweet_counter += 1
+		file.write(tweet.full_text.encode("ascii", "ignore"))
+		file.write("\n")
+		last = tweet
+	last_id = last.id_str
+
+	counter += 1
+	tweets = api.user_timeline(screen_name=handle, count=200, max_id=last_id, include_rts = False, tweet_mode='extended')
+
+print("Tweets Ingested: " + str(tweet_counter))
